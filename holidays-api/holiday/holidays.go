08fae283c40569bfb07563b243f9ceff3d3be4c6
Ichinose Shogo <shogo82148@gmail.com>
2021-08-22 17:50:33 +0900

    implement findHolidaysInMonth

--- holidays-api/holiday/holidays.go
+++ holidays-api/holiday/holidays.go
@@ -11,6 +11,7 @@ type Holiday struct {
 	Name string
 }
 
+// findHoliday returns whether the specific day is a holiday.
 func findHoliday(year int, month time.Month, day int) (Holiday, bool) {
 	date := fmt.Sprintf("%04d-%02d-%02d", year, int(month), day)
 	idx := sort.Search(len(holidays), func(i int) bool {
@@ -21,4 +22,18 @@ func findHoliday(year int, month time.Month, day int) (Holiday, bool) {
 		return holidays[idx], true
 	}
 	return Holiday{}, false
+}
+
+// findHolidaysInMonth returns holidays in the specific month.
+func findHolidaysInMonth(year int, month time.Month) []Holiday {
+	startDate := fmt.Sprintf("%04d-%02d-01", year, int(month))
+	endDate := fmt.Sprintf("%04d-%02d-99", year, int(month))
+
+	start := sort.Search(len(holidays), func(i int) bool {
+		return holidays[i].Date >= startDate
+	})
+	end := sort.Search(len(holidays), func(i int) bool {
+		return holidays[i].Date >= endDate
+	})
+	return holidays[start:end]
 }
--- holidays-api/holiday/holidays_test.go
+++ holidays-api/holiday/holidays_test.go
@@ -3,6 +3,8 @@ package holiday
 import (
 	"testing"
 	"time"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestFindHoliday(t *testing.T) {
@@ -12,5 +14,22 @@ func TestFindHoliday(t *testing.T) {
 	}
 	if got, want := h.Date, "2000-01-01"; want != got {
 		t.Errorf("want %q, got %q", want, got)
+	}
+}
+
+func TestFindHolidaysInMonth(t *testing.T) {
+	got := findHolidaysInMonth(2000, time.January)
+	want := []Holiday{
+		{
+			Date: "2000-01-01",
+			Name: "元日",
+		},
+		{
+			Date: "2000-01-10",
+			Name: "成人の日",
+		},
+	}
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("holidays not match: (-want/+got)\n%s", diff)
 	}
 }
